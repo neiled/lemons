3276b66e2273a220bbacfb3cf6a859b8c5b3a955
Neil Edwards <neil@plasticwater.com>
2016-01-11 21:39:59 -0500

    business module

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,14 +1,28 @@
 import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+// import increaseStock from '../redux/modules/business'
+import { actions as businessActions } from '../redux/modules/business'
 
+const mapStateToProps = (state) => ({
+  counter: state.business.counter
+})
 class Business extends React.Component {
   static propTypes = {
-    name: PropTypes.string.isRequired
+    name: PropTypes.string.isRequired,
+    counter: PropTypes.number.isRequired,
+    incrementStock: PropTypes.func.isRequired
   };
   render () {
     return (
-    <p>{this.props.name}</p>
+    <div>
+      <p>{this.props.name}</p>
+      <p>{this.props.counter}</p>
+      <button className='btn btn-default'
+              onClick={() => this.props.incrementStock(1)}>
+        Buy Stock
+      </button>
+    </div>
     )
   }
 }
-
-export default Business
+export default connect(mapStateToProps, businessActions)(Business)
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { routeReducer as router } from 'redux-simple-router'
 import counter from './modules/counter'
+import business from './modules/business'
 
 export default combineReducers({
   counter,
+  business,
   router
 })
